e25cff8c2e00e7727c27f0eec5caaa66873d980d
brockBjerke <159338581+brockBjerke@users.noreply.github.com>
2024-04-18 14:41:57 -0500

    WORKING WITH ACTUAL XML AND REAL TREE PICTURES

--- carouselController.js
+++ carouselController.js
@@ -5,36 +5,28 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach(button => {
     // Iterate over each button in the buttons NodeList using forEach method
-
+    // Add a click event listener to each button
     button.addEventListener("click", () => {
-        // Add a click event listener to each button
-
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
         // Check if the clicked button has a data-carousel-button attribute with the value "next"
         // If yes, set offset to 1, otherwise set it to -1
-
-        const slides = button.closest("[data-carousel]").querySelector('[data-slides]');
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
         // Find the closest ancestor element of the clicked button that has a data-carousel attribute
         // Then, find the element with the attribute data-slides within that ancestor
-
-        const activeSlide = slides.querySelector("[data-active]");
+        const slides = button.closest("[data-carousel]").querySelector('[data-slides]');
         // Find the slide with the attribute data-active within the slides element
         // This represents the currently active slide in the carousel
-
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        // Find the index of the active slide within the slides element
+        const activeSlide = slides.querySelector("[data-active]");
+          // Find the index of the active slide within the slides element
         // Then, adjust the index by the offset to determine the index of the next slide
-
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+      // If the new index is less than 0, loop back to the last slide
         if (newIndex < 0) newIndex = slides.children.length - 1;
-        // If the new index is less than 0, loop back to the last slide
-
-        if (newIndex >= slides.children.length) newIndex = 0;
         // If the new index is greater than or equal to the number of slides, loop back to the first slide
-
+        if (newIndex >= slides.children.length) newIndex = 0;
+          // Set the data-active attribute of the next slide to true, making it the active slide
         slides.children[newIndex].dataset.active = true;
-        // Set the data-active attribute of the next slide to true, making it the active slide
-
-        delete activeSlide.dataset.active;
         // Remove the data-active attribute from the currently active slide
+        delete activeSlide.dataset.active;
+        
     });
 });
