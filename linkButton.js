f04dae047f1768c48274d7b61adec2759a85db79
brockBjerke <159338581+brockBjerke@users.noreply.github.com>
2024-04-23 16:10:56 -0500

    map button now works

--- linkButton.js
+++ linkButton.js
@@ -14,9 +14,9 @@ function pullFromLinkSlideMap() {
     console.log("mapButton");
     let currentSlide = document.querySelector('.slide[data-active]');
     if (currentSlide) {
-        let idname = currentSlide.querySelector('p').id;
-        if (idname) {
-            window.location.href = idname;
+        let secondLink = currentSlide.querySelector('p:nth-child(2)').id;
+        if (secondLink) {
+            window.location.href = secondLink;
         }
     }
 }
