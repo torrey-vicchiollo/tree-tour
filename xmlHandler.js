223a325193a3c38a87cbe577b413b286289f08f6
brockBjerke <159338581+brockBjerke@users.noreply.github.com>
2024-04-23 12:06:15 -0500

    working tour carousel

--- xmlHandler.js
+++ xmlHandler.js
@@ -1,16 +1,16 @@
-function loadXMLDoc() {
+function loadXMLDocForCarousel() {
     //Code to do XML HTTP request (see slides) goes here
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {      // Action to be performed when the document is read;
-            myFunction(this);
+            fillHomeCarousel(this);
         }
     };
     xhttp.open("GET", "1.xml", true);
     xhttp.send();
 }
 
-function myFunction(xml) {
+function fillHomeCarousel(xml) {
     let x, i, xmlDoc, txt;
     xmlDoc = xml.responseXML; //Response returned as XML data
     txt = "";
@@ -27,7 +27,7 @@ function myFunction(xml) {
         // Check if both image source and geo location exist
         if (imagePath && geoLocation && imageName != "Rock Elm") { //getting rid of rock elm cause it doesnt work
             // Create li element
-            
+
             let li = document.createElement('li');
             li.classList.add('slide');
 
@@ -35,12 +35,13 @@ function myFunction(xml) {
             let img = document.createElement('img');
             img.src = imagePath;
             img.alt = x[i].getElementsByTagName("DisplayName")[0].innerHTML.trim();
+            li.textContent = img.alt;
 
             // Append img to li
             li.appendChild(img);
             if (i === 0) {
-                            li.dataset.active = true;
-                        }
+                li.dataset.active = true;
+            }
             // Append li to ul
             slides.appendChild(li);
 
@@ -48,4 +49,60 @@ function myFunction(xml) {
     }
     // Set the first slide as active
     slides.querySelector('.slide').classList.add('active');
+}
+
+function loadXMLDocForTours() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) { // Check if the request is complete
+            if (this.status == 200) { // Check if the request was successful
+                console.log("XML response:", this.responseText); // Log the XML response
+                fillTourCarousel(this);
+            } else {
+                console.error("Failed to load XML data. Status code: " + this.status);
+            }
+        }
+    };
+    xhttp.open("GET", "2.xml", true);
+    xhttp.send();
+}
+
+
+function fillTourCarousel(xml) {
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(xml.responseText, "text/xml");
+
+    let x = xmlDoc.getElementsByTagName("ThemeData");
+    let slides = document.querySelector('.carousel [data-slides]');
+
+    if (!slides) {
+        console.error("Slides element not found");
+        return;
+    }
+
+    for (let i = 0; i < x.length; i++) {
+        let themeName = x[i].getElementsByTagName("ThemeName")[0];
+        let description = x[i].getElementsByTagName("Description")[0];
+        let themeId = x[i].getElementsByTagName("ThemeId")[0];
+
+        if (themeName && description && themeId) {
+            let themeNameText = themeName.innerHTML.trim();
+            let descriptionText = description.innerHTML.trim();
+            let themeIdText = themeId.innerHTML.trim();
+
+            let a = document.createElement('a');
+            a.href = "https://www2.winona.edu/m/arboretum/directory.asp?t=" + themeIdText;
+            a.textContent = themeNameText + " " + descriptionText;
+
+            let li = document.createElement('li');
+            li.classList.add('slide');
+            li.appendChild(a);
+
+            slides.appendChild(li);
+
+            if (i === 0) {
+                li.dataset.active = true;
+            }
+        }
+    }
 }
